d184bbb9d9eaa085bed5642a24d117abf82ba05f
latviancoder <igvadaimon@gmail.com>
2021-09-25 17:54:10 +0200

    fix server

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
+app.get('/api', (req, res) => {
+  res.send('API');
 });
 
 app.listen(port, () => {
