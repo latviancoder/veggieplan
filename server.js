253b460330abff1aa45c8aad6c30943945e630aa
latviancoder <igvadaimon@gmail.com>
2021-09-25 17:50:15 +0200

    serve /build from express

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
 app.get('/hello', (req, res) => {
   res.send('Hello World!');
 });
