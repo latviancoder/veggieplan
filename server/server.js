d59b7cf923a1f2e1f4fc660469defaae80ce491b
latviancoder <igvadaimon@gmail.com>
2022-07-12 17:36:33 +0200

    proper saving of varieties

--- server/server.js
+++ server/server.js
@@ -61,19 +61,34 @@ app.get('/api/varieties', async (req, res) => {
   res.json(camelCaseObjectDeep(result.rows));
 });
 
-// TODO make this more efficient, removing everything is dumb
 app.put('/api/varieties', async (req, res) => {
-  await client.query(SQL`DELETE FROM varieties`);
+  const newVarieties = req.body || [];
+  const newVarietiesIds = newVarieties.map(({ id }) => id);
+
+  const prevVarieties = await client.query(SQL`SELECT * FROM varieties`);
+
+  const deletedVarietiesIds = prevVarieties.rows
+    .filter((prev) => !newVarietiesIds.includes(prev.id))
+    .map(({ id }) => id);
+
+  if (deletedVarietiesIds.length) {
+    await client.query(
+      `UPDATE objects SET variety_id = NULL WHERE variety_id = ANY($1)`,
+      [deletedVarietiesIds]
+    );
+
+    await client.query(`DELETE FROM varieties WHERE id = ANY($1)`, [
+      deletedVarietiesIds,
+    ]);
+  }
 
   for (const obj of req.body || []) {
-    console.log(SQL`INSERT INTO varieties
-    (id, plant_id, name, row_spacing, in_row_spacing, maturity) VALUES
-    (${obj.id}, ${obj.plantId}, ${obj.name}, ${obj.rowSpacing}, ${obj.inRowSpacing}, ${obj.maturity})`);
-
     await client.query(
       SQL`INSERT INTO varieties
       (id, plant_id, name, row_spacing, in_row_spacing, maturity) VALUES
-      (${obj.id}, ${obj.plantId}, ${obj.name}, ${obj.rowSpacing}, ${obj.inRowSpacing}, ${obj.maturity})`
+      (${obj.id}, ${obj.plantId}, ${obj.name}, ${obj.rowSpacing}, ${obj.inRowSpacing}, ${obj.maturity})
+      ON CONFLICT (id) DO UPDATE SET
+        name = ${obj.name}, row_spacing = ${obj.rowSpacing}, in_row_spacing = ${obj.inRowSpacing}, maturity = ${obj.maturity}`
     );
   }
 
