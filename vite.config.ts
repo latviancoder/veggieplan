b9f73bafd259503d0c7001da77a70149777cc237
latviancoder <igvadaimon@gmail.com>
2022-07-21 14:22:07 +0200

    eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import svgr from "vite-plugin-svgr";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
-    outDir: "build",
+    outDir: 'build',
   },
   plugins: [
     svgr(),
     react(),
     tsconfigPaths({
-      extensions: [".ts", ".tsx", ".js", ".jsx", ".mjs", ".scss"],
+      extensions: ['.ts', '.tsx', '.js', '.jsx', '.mjs', '.scss'],
     }),
   ],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:5000",
+      '/api': {
+        target: 'http://localhost:5000',
         changeOrigin: true,
       },
     },
