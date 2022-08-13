e10974d55f669e566e63b93feb0380e8bd73dbe0
latviancoder <igvadaimon@gmail.com>
2022-08-13 19:48:02 +0200

    add auth0

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { HotkeysProvider } from '@blueprintjs/core';
 import { ReactElement, Suspense } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { useAtomsDevtools } from 'jotai/devtools';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 import Root from './components/root/Root';
 
@@ -19,13 +20,19 @@ const AtomsDevtools = ({ children }: { children: ReactElement }) => {
 };
 
 const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <HotkeysProvider>
-      <Suspense fallback="Es wird geladen...">
-        <Root />
-      </Suspense>
-    </HotkeysProvider>
-  </QueryClientProvider>
+  <Auth0Provider
+    domain="veggieplan.eu.auth0.com"
+    clientId="vGq3zTKCkjUGZMgtieYDD2azr2P5tx8D"
+    redirectUri={window.location.origin}
+  >
+    <QueryClientProvider client={queryClient}>
+      <HotkeysProvider>
+        <Suspense fallback="Es wird geladen...">
+          <Root />
+        </Suspense>
+      </HotkeysProvider>
+    </QueryClientProvider>
+  </Auth0Provider>
 );
 
 export default App;
